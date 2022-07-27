c12cca7bf958cf6a83fb89de1ccda74895804710
Zambo-dev <thomasz01@outlook.it>
2022-07-27 22:38:58 +0200

    Implemented norm 1, 2 and inf Implemented equals overloading

--- src/main/kotlin/net/davoleo/javajuice/math/vector/Vector2d.kt
+++ src/main/kotlin/net/davoleo/javajuice/math/vector/Vector2d.kt
@@ -1,5 +1,9 @@
 package net.davoleo.javajuice.math.vector
 
+import kotlin.math.max
+import kotlin.math.pow
+import kotlin.math.sqrt
+
 class Vector2d(a: Double, b: Double) : Vector {
 
     private val vec: DoubleArray
@@ -12,10 +16,23 @@ class Vector2d(a: Double, b: Double) : Vector {
         get() = 2
 
     override fun norm(type: Int): Double {
-        TODO("Not yet implemented")
+        // Norma 1
+        return when (type) {
+            1 -> vec[0] + vec[1]
+            // Norma 2
+            2 -> sqrt(vec[0].pow(2.0) + vec[1].pow(2.0))
+            // Norma inf
+            else -> max(vec[0], vec[1])
+        }
     }
 
     override fun scale(multiplier: Double): Vector {
-        return Vector2d(vec[0] * multiplier, vec[1] * multiplier);
+        return Vector2d(vec[0] * multiplier, vec[1] * multiplier)
+    }
+
+    override fun equals(other: Any?): Boolean {
+        if(other !is Vector2d)
+            return false
+        return (vec[0] == other.vec[0] && vec[1] == other.vec[1])
     }
 }
