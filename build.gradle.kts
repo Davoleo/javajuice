380bfb247b3edd54c6f70d23b14e2c467a739b7a
Zambo-dev <thomasz01@outlook.it>
2022-07-28 10:23:58 +0200

    Removed duplicate

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,7 +14,6 @@ dependencies {
     implementation(kotlin("stdlib"))
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.6.0")
     testImplementation("org.junit.jupiter:junit-jupiter:5.8.1")
-    testImplementation("org.junit.jupiter:junit-jupiter:5.8.1")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
 }
 
